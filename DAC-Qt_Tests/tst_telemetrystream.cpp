4072615203f15290d56307fc21e037f8728d7e6e
ahthae <ahthaeon@pm.me>
2023-11-02 20:47:58 -0700

    Add simple dispatching channel

--- DAC-Qt_Tests/tst_telemetrystream.cpp
+++ DAC-Qt_Tests/tst_telemetrystream.cpp
@@ -9,35 +9,14 @@ using namespace testing;
 
 TEST(TelemetryStreamTest, ChannelCreateInputBuffer)
 {
-  TelemetrySink<PressureData> sink(50);
-  TelemetryChannel<PressureData> channel;
-
-  sink.register_channel(&channel);
-  std::vector<TelemetryMessage<PressureData>*> buffer = channel.get_input_buffer(&sink);
-
-  ASSERT_THAT(buffer.size(), Eq(50));
-}
-
-TEST(TelemetryStreamTest, ChannelCallsSubscriberCallback) {
-  bool is_called = false;
-
-  TelemetrySink<double> sink(1);
-  TelemetryChannel<double> channel;
-  channel.subscribe([&is_called](TelemetryChannel<double> *channel, const size_t size) {
-    auto it = channel->get_read_iterator();
-    double num = (*it)->data;
-    std::cout << num << ' ';
-    EXPECT_DOUBLE_EQ(num, 14.0);
-    is_called = true;
+  TelemetryChannel<TelemetryMessage<PressureData>> channel;
+  channel.subscribe([](const std::vector<TelemetryMessage<PressureData>>& buffer, const size_t size) {
+    ASSERT_TRUE(true);
   });
-  sink.register_channel(&channel);
-  std::vector<TelemetryMessage<double>*> &buffer = channel.get_input_buffer(&sink);
-  buffer[0] = new TelemetryMessage<double>;
-  buffer[0]->data = 14.0;
-  channel.clear_buffer(&sink);
-  buffer[0] = new TelemetryMessage<double>;
-  buffer[0]->data = 15.0;
-  channel.clear_buffer(&sink);
 
-  ASSERT_THAT(is_called, Eq(true));
+  std::vector<TelemetryMessage<PressureData>> buffer;
+  TelemetryMessage<PressureData> msg;
+  msg.data.pressure = 14;
+  buffer.push_back(msg);
+  channel.broadcast(buffer, buffer.size());
 }
--- TelemetryChannel.h
+++ TelemetryChannel.h
@@ -11,81 +11,33 @@ public:
   T data;
 };
 
-template <class T> class TelemetrySink;
-
 template <class T>
 class TelemetryChannel
 {
 public:
-  TelemetryChannel() : message_queue_(default_queue_size) {
-    message_queue_.resize(50);
-    write_ = message_queue_.begin();
-    read_ = message_queue_.begin();
-  }
-
-  /* Output */
-
-  inline void subscribe(std::function<void(TelemetryChannel<T>*, const size_t)> callback) {
+  inline void subscribe(std::function<void(const std::vector<T>&, const size_t)> callback) {
     callbacks_.push_back(callback);
   }
-  inline typename std::vector<TelemetryMessage<T>*>::const_iterator get_read_iterator() { return read_; }
 
-  /* Input */
-
-  inline void create_input_buffer(const TelemetrySink<T>* sink) {
-    buffers_[sink].resize(sink->get_buffer_size());
-  }
-  inline std::vector<TelemetryMessage<T>*>& get_input_buffer(const TelemetrySink<T>* sink) { return buffers_[sink]; }
-  inline void clear_buffer(const TelemetrySink<T>* sink, size_t size = 0) {
-    auto buffer = get_input_buffer(sink);
-    if (size == 0) {
-      size = buffer.size();
+  inline void broadcast(const std::vector<T> &buffer, const size_t size) const noexcept {
+    for (const auto& callback : callbacks_) {
+      callback(buffer, size);
     }
-
-    std::move(buffer.begin(), buffer.begin()+size, write_);
-
-    for (const auto &callback: callbacks_) {
-      callback(this, size);
-    }
-
-    write_ += size;
-    read_ += size;
   }
-
-  // Approximately 3x the storage needed for 60 seconds of data at 1 kHz (wanted something arbitrarily large)
-  static constexpr size_t default_queue_size = 1000*sizeof(TelemetryMessage<double>)*60*3;
 
 private:
   std::vector<
-    std::function<void(TelemetryChannel<T>*, const size_t)>
+    std::function<void(const std::vector<T>&, const size_t)>
     > callbacks_;
-
-  // TODO guaranteed deterministic buffer or double buffering
-  std::vector<TelemetryMessage<T>*> message_queue_;
-  typename std::vector<TelemetryMessage<T>*>::const_iterator read_;
-  typename std::vector<TelemetryMessage<T>*>::iterator write_;
-
-  std::map<
-    const TelemetrySink<T>*,
-    std::vector<TelemetryMessage<T>*>
-    > buffers_;
 };
 
 template <class T>
 class TelemetrySink {
 public:
-  TelemetrySink(const size_t buffer_size) : buffer_size_{buffer_size} { }
+  TelemetrySink(TelemetryChannel<T>* channel) : channel_{channel} { }
   virtual ~TelemetrySink() =default;
-  virtual size_t get_buffer_size() const noexcept { return buffer_size_; }
-  virtual TelemetryChannel<T>* get_channel() const noexcept { return channel_; }
-  void register_channel(TelemetryChannel<T>* channel) {
-    channel->create_input_buffer(this);
-    channel_ = channel;
-  }
-private:
-  size_t buffer_size_;
+protected:
   TelemetryChannel<T>* channel_ {nullptr};
 };
-
 
 #endif // TELEMETRYCHANNEL_H
