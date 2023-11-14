f194ab2ab96fcab509f05cc926fc4e890fea9ba1
zanderdo <adobm001@ucr.edu>
2023-11-13 17:01:01 -0800

    added initial member variables and methods

--- launchinhibitor.cpp
+++ launchinhibitor.cpp
@@ -4,3 +4,48 @@ LaunchInhibitor::LaunchInhibitor()
 {
 
 }
+
+bool LaunchInhibitor::checkAll(){
+    if (checkNOSTank() && checkEthTank() && checkInjector() && checkChamber()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool LaunchInhibitor::checkNOSTank(){
+    if (nosTankLoad && (nosTankPres > 750 && nosTankPres < 850) && nosTankTemp) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool LaunchInhibitor::checkEthTank(){
+    if (ethTankLoad && (nosTankPres > 750 && nosTankPres < 850) && ethTankTemp) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool LaunchInhibitor::checkInjector(){
+    if (ethInjectorPres && nosInjectorPres) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool LaunchInhibitor::checkChamber(){
+    if (chamberPres && chamberTemp1 && chamberTemp2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- launchinhibitor.h
+++ launchinhibitor.h
@@ -6,6 +6,38 @@ class LaunchInhibitor
 {
 public:
     LaunchInhibitor();
+
+    //PTs
+    double chamberPres;
+    double ethInjectorPres;
+    double nosInjectorPres;
+    double ethTankPres;
+    double nosTankPres;
+
+    //LCs
+    double thrustLoad;
+    double ethTankLoad;
+    double nosTankLoad;
+
+    //TCs
+    double chamberTemp1;
+    double chamberTemp2;
+    double ethTankTemp;
+    double nosTankTemp;
+
+    //bools
+    bool nosTankGood;
+    bool ethTankGood;
+    bool injectorGood;
+    bool chamberGood;
+
+    //Functions
+    bool checkAll();
+    bool checkNOSTank();
+    bool checkEthTank();
+    bool checkInjector();
+    bool checkChamber();
+
 };
 
 #endif // LAUNCHINHIBITOR_H
