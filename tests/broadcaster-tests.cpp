7896f7b2417d16362cf0c7ed3d9e0c5740483fde
ahthae <ahthaeon@pm.me>
2024-01-01 15:49:24 -0800

    Add sink unit test

--- tests/broadcaster-tests.cpp
+++ tests/broadcaster-tests.cpp
@@ -1,17 +1,44 @@
 #include <gtest/gtest.h>
 
 #include "../broadcast/Broadcaster.h"
+#include "../broadcast/Sink.h"
+#include "../models/ForceData.h"
 
-void callback(const TelemetryData* data) {
-  EXPECT_NE(data, nullptr);
-  ASSERT_EQ(data->label, "Test");
+class BroadcasterTest : public testing::Test{
+protected:
+  void SetUp() override {
+    data.label = "Test";
+    forceData.label = "Force Test";
+    broadcaster->subscribe(callback);
+    forceBroadcaster->subscribe(forceCallback);
+  }
+
+  std::shared_ptr<Broadcaster<TelemetryData>> broadcaster = Broadcaster<TelemetryData>::getInstance();
+  std::shared_ptr<Broadcaster<ForceData>> forceBroadcaster = Broadcaster<ForceData>::getInstance();
+
+  TelemetryData data;
+  ForceData forceData;
+
+  static void callback(const TelemetryData* data) {
+    EXPECT_NE(data, nullptr);
+    ASSERT_EQ(data->label, "Test");
+  }
+  static void forceCallback(const ForceData* data) {
+    EXPECT_NE(data, nullptr);
+    ASSERT_EQ(data->label, "Force Test");
+  }
+};
+
+TEST_F(BroadcasterTest, ShouldBroadcast) {
+  broadcaster->broadcastBase(&data);
 }
 
-TEST(BroadcasterTest, ShouldBroadcast) {
-  std::shared_ptr<BroadcasterBase> broadcaster = Broadcaster<TelemetryData>::getInstance();
-  std::dynamic_pointer_cast<Broadcaster<TelemetryData>>(broadcaster)->subscribe(&callback);
-  auto data = new TelemetryData();
-  data->label = "Test";
+TEST_F(BroadcasterTest, SinkShouldSend) {
+  Sink sink({broadcaster});
+  sink.sendData({&data});
+}
 
-  broadcaster->broadcastBase(data);
+TEST_F(BroadcasterTest, SinkShouldSendTyped) {
+  Sink sink({forceBroadcaster});
+  sink.sendData({&forceData});
 }
