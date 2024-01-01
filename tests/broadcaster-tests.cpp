5b93135b7e08d811f5918c1593514c1d97f71603
ahthae <ahthaeon@pm.me>
2024-01-01 14:56:33 -0800

    Add broadcaster unit test

--- tests/broadcaster-tests.cpp
+++ tests/broadcaster-tests.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 
-TEST(DAC_Qt_test, Test) {
-  ASSERT_EQ(1,1);
+#include "../broadcast/Broadcaster.h"
+
+void callback(const TelemetryData* data) {
+  EXPECT_NE(data, nullptr);
+  ASSERT_EQ(data->label, "Test");
+}
+
+TEST(BroadcasterTest, ShouldBroadcast) {
+  std::shared_ptr<BroadcasterBase> broadcaster = Broadcaster<TelemetryData>::getInstance();
+  std::dynamic_pointer_cast<Broadcaster<TelemetryData>>(broadcaster)->subscribe(&callback);
+  auto data = new TelemetryData();
+  data->label = "Test";
+
+  broadcaster->broadcastBase(data);
 }
