e0cf1c75edaadfe08df89836ae4e25fc2ca251f3
ahthae <ahthaeon@pm.me>
2024-01-10 18:20:46 -0800

    Remove LabJackSink test snippet

--- main.cpp
+++ main.cpp
@@ -5,22 +5,12 @@
 #include "models/ForceData.h"
 #include "models/TemperatureData.h"
 #include "models/PressureData.h"
-#include "sinks/LabJackSink.h"
-#include "sinks/LabJackSinkStrategy.h"
 
 int main(int argc, char *argv[])
 {
   auto forceBroadcaster = Broadcaster<ForceData>::getInstance();
   auto temperatureBroadcaster = Broadcaster<TemperatureData>::getInstance();
   auto pressureBroadcaster = Broadcaster<PressureData>::getInstance();
-
-  pressureBroadcaster->subscribe([](const PressureData* data){
-    std::cout << data->label << ": " << data->pressure() << std::endl;
-  });
-
-  LabJackSink labJackSink;
-  labJackSink.openS("ANY", "ANY", "ANY");
-  labJackSink.start_stream(1, 1, std::make_shared<InjectorTestStrategy>(pressureBroadcaster));
 
   QApplication app(argc, argv);
 
