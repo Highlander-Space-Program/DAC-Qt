b3e4e58d355118ed1b3bb43329e626cb807bc231
ahthae <ahthaeon@pm.me>
2024-01-12 14:57:50 -0800

    Build with influxdb-cxx

--- main.cpp
+++ main.cpp
@@ -6,11 +6,20 @@
 #include "models/TemperatureData.h"
 #include "models/PressureData.h"
 
+#include <InfluxDBFactory.h>
+
 int main(int argc, char *argv[])
 {
   auto forceBroadcaster = Broadcaster<ForceData>::getInstance();
   auto temperatureBroadcaster = Broadcaster<TemperatureData>::getInstance();
   auto pressureBroadcaster = Broadcaster<PressureData>::getInstance();
+
+  auto db = influxdb::InfluxDBFactory::Get("http://localhost:8086/?db=test");
+  pressureBroadcaster->subscribe([&db](const PressureData *data){
+      db->write(influxdb::Point{"test"}
+      .addField("value", 10)
+      .addTag("host", "localhost"));
+  });
 
   QApplication app(argc, argv);
 
