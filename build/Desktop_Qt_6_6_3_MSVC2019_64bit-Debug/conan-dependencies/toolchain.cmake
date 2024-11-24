77d43f838b0a5625ea946edfe7ff6b30ff436c15
sudoNeo <mlopez4059@panther.chaffey.edu>
2024-11-23 20:07:54 -0800

    Ok fixed it this time i think

--- build/Desktop_Qt_6_6_3_MSVC2019_64bit-Debug/conan-dependencies/toolchain.cmake
+++
@@ -1,4 +0,0 @@
-
-        set(CMAKE_C_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.39.33519/bin/HostX64/x64/cl.exe")
-        set(CMAKE_CXX_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.39.33519/bin/HostX64/x64/cl.exe")
-        
--- build/Desktop_Qt_6_6_3_MSVC2019_64bit-Release/conan-dependencies/build/conan/conandeps_legacy.cmake
+++
@@ -1,8 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(nlohmann_json)
-find_package(spdlog)
-find_package(InfluxDB)
-
-set(CONANDEPS_LEGACY  nlohmann_json::nlohmann_json  spdlog::spdlog  InfluxData::InfluxDB )
